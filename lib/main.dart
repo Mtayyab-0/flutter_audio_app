1fa14db988e8caa68100c984b43daf5f0ef224c7
Mtayyab-0 <tayyab8505@gmail.com>
2022-03-04 00:34:50 +0500

    Updated packages, uploading audio file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/playing_audio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AudioPlay extends StatefulWidget {
+  const AudioPlay({Key? key}) : super(key: key);
+
+  @override
+  State<AudioPlay> createState() => _AudioPlayState();
+}
+
+class _AudioPlayState extends State<AudioPlay> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
